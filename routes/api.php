f7fce5d03a0b07483463bf406e5609871d857d35
mr diy <maspiras@gmail.com>
2025-09-12 06:30:17 +0800

    api route for register and login

--- routes/api.php
+++ routes/api.php
@@ -13,9 +13,9 @@ use App\Http\Controllers\AuthController;
 /* Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']); */
 
-Route::get('/register', [AuthController::class, 'register']);
+/* Route::get('/register', [AuthController::class, 'register']);
 Route::get('/login', [AuthController::class, 'login']);
-
+ */
 // Protected routes
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+// Protected routes
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::get('/profile', [AuthController::class, 'profile']);
+});
+

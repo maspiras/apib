7518b4af28dcd42c0fde1e342f998a9c1e7d431b
maspiras <mcaspiras@yahoo.com>
2025-10-23 05:27:13 +0800

    added middleware verifymobileapp

---
+++ app/Http/Middleware/VerifyMobileApp.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class VerifyMobileApp
+{
+      /**
+       * Handle an incoming request.
+       *
+       * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+       */
+      public function handle(Request $request, Closure $next): Response
+      {
+            iif ($request->header('X-API-KEY') !== config('app.app_key_mobile')) {
+                  return response()->json(['message' => 'Invalid mobile application key'], 403);
+            }
+            return $next($request);
+      }
+}

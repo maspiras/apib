5a10e83729c28a0af0b63733e8d9222261922544
maspiras <mcaspiras@yahoo.com>
2025-10-25 16:24:00 +0800

    bug fixes on route api

--- routes/api.php
+++ routes/api.php
@@ -16,6 +16,14 @@ use Laravel\Sanctum\Sanctum;
 use App\Http\Controllers\Rooms\RoomController;
 use App\Http\Controllers\Users\UserController;
 use App\Http\Controllers\Reservations\ReservationController;
+
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
+use App\Http\Controllers\Auth\EmailVerificationNotificationController;
+use App\Http\Controllers\Auth\NewPasswordController;
+use App\Http\Controllers\Auth\PasswordResetLinkController;
+use App\Http\Controllers\Auth\RegisteredUserController;
+use App\Http\Controllers\Auth\VerifyEmailController;
+
 /* Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
@@ -44,4 +52,32 @@ Route::middleware(['auth:sanctum', 'throttle:60,1', 'usertimezone'])->group(func
     Route::apiResource('reservations', ReservationController::class);
     //});
 });
-require __DIR__ . '/auth.php';
+//require __DIR__ . '/auth.php';
+
+Route::post('/register', [RegisteredUserController::class, 'store'])
+    ->middleware('guest')
+    ->name('register');
+
+Route::post('/login', [AuthenticatedSessionController::class, 'store'])
+    //  ->middleware('guest')
+    ->name('login');
+
+Route::post('/forgot-password', [PasswordResetLinkController::class, 'store'])
+    ->middleware('guest')
+    ->name('password.email');
+
+Route::post('/reset-password', [NewPasswordController::class, 'store'])
+    ->middleware('guest')
+    ->name('password.store');
+
+Route::get('/verify-email/{id}/{hash}', VerifyEmailController::class)
+    ->middleware(['auth', 'signed', 'throttle:6,1'])
+    ->name('verification.verify');
+
+Route::post('/email/verification-notification', [EmailVerificationNotificationController::class, 'store'])
+    ->middleware(['auth', 'throttle:6,1'])
+    ->name('verification.send');
+
+Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])
+    ->middleware('auth')
+    ->name('logout');
